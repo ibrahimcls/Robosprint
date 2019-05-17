0c7c9413f559c024a0b741fd67b1c55095e87c45
İbrahim <34775815+ibrahimcls@users.noreply.github.com>
2019-05-17 12:09:02 +0300

    remote controllable vehicle
    remote controllable vehicle we designed at Robosprint contest organized in trabzon

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Robosprint
+
+# Constants that are already predefined
+D0->16
+D1->5
+D2->4
+D3->0
+D4->2
+D5->14
+D6->12
+D7->13
+D8->15
+A0->17
+
+ # Required hardware
+motorcycle driver
+NodeMCU micricontroller
+4 motors and 4 wheels
+2 plate,a led, screw and some cable
---
+++ Robosprint/Robosprint.ino
@@ -0,0 +1,192 @@
+#define ENA   2          // Enable/speed motors Right        (D4)
+#define ENB   15          // Enable/speed motors Left         (D8)
+#define IN_1  13          // L298N in1 motors Right           (D7)
+#define IN_2  0          // L298N in2 motors Right           (D3)
+#define IN_3  4           // L298N in3 motors Left           (D2)
+#define IN_4  5           // L298N in4 motors Left          (D1)
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
+String command;             //String to store app command state.
+int speedCar = 800;   //high      // 400 - 1023.
+int speed_Coeff = 3;
+
+const char* ssid = "emir";
+const char* password="emir123456";
+ESP8266WebServer server(80);
+
+void setup() {
+ 
+ pinMode(ENA, OUTPUT);
+ pinMode(ENB, OUTPUT);  
+ pinMode(IN_1, OUTPUT);
+ pinMode(IN_2, OUTPUT);
+ pinMode(IN_3, OUTPUT);
+ pinMode(IN_4, OUTPUT); 
+ pinMode(16, OUTPUT); //D0 yesil
+  
+  Serial.begin(115200);
+  
+// Connecting WiFi
+
+  WiFi.mode(WIFI_AP);
+  //WiFi.softAP(ssid);
+  WiFi.begin(ssid,password);
+
+  //IPAddress myIP = WiFi.softAPIP();
+  IPAddress myIP = WiFi.localIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+ 
+ // Starting WEB-server 
+     server.on ( "/", HTTP_handleRoot );
+     server.onNotFound ( HTTP_handleRoot );
+     server.begin();    
+}
+
+void goAhead(){ 
+  
+      digitalWrite(16, HIGH);
+       
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+  }
+
+void goBack(){ 
+      digitalWrite(16, LOW);
+    
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goRight(){ 
+      digitalWrite(16, LOW);
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goLeft(){
+      digitalWrite(16, LOW);
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2,LOW);
+      analogWrite(ENA, speedCar);
+       
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+  }
+
+void goAheadLeft(){
+  
+      digitalWrite(16, HIGH);
+      
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar/speed_Coeff);
+ 
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+   }
+
+void goAheadRight(){
+      digitalWrite(16, HIGH);
+      
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar/speed_Coeff);
+  }
+
+void goBackLeft(){ 
+       digitalWrite(16, LOW);
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar/speed_Coeff);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goBackRight(){ 
+      digitalWrite(16, LOW);
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar/speed_Coeff);
+  }
+
+void stopRobot(){  
+       digitalWrite(16, LOW);
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+ }
+
+void loop() {
+    server.handleClient();
+    
+      command = server.arg("State");
+      if (command == "F") goAhead();
+      else if (command == "B") goBack();
+      else if (command == "L") goLeft();
+      else if (command == "R") goRight();
+      else if (command == "I") goAheadRight();
+      else if (command == "G") goAheadLeft();
+      else if (command == "J") goBackRight();
+      else if (command == "H") goBackLeft();
+      else if (command == "0") speedCar = 400;
+      else if (command == "1") speedCar = 470;
+      else if (command == "2") speedCar = 540;
+      else if (command == "3") speedCar = 610;
+      else if (command == "4") speedCar = 680;
+      else if (command == "5") speedCar = 750;
+      else if (command == "6") speedCar = 820;
+      else if (command == "7") speedCar = 890;
+      else if (command == "8") speedCar = 960;
+      else if (command == "9") speedCar = 1023;
+      else if (command == "S") stopRobot();
+      
+}
+
+void HTTP_handleRoot(void) {
+
+if( server.hasArg("State") ){
+       Serial.println(server.arg("State"));
+  }
+  server.send ( 200, "text/html", "" );
+  delay(1);
+}
